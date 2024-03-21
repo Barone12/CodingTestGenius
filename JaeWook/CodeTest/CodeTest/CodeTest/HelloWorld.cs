90b4519ced2da1cff4f0c8bb2675cc0d86878285
Jeong Jae Wook <jgy4846@naver.com>
2024-03-22 01:40:24 +0900

    solve basic tests

---
+++ JaeWook/CodeTest/CodeTest/CodeTest/HelloWorld.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainCode
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+        }
+
+    }
+}
